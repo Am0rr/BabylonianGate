aaa6e7805286b4f4ac716375f6490cf235b3fcd6
Am0rr <itsaizel468@gmail.com>
2026-01-19 17:19:05 +0200

    feat(app): implement Create and Update validator for Soldier entity

---
+++ BG.App/Validators/Soldiers/CreateSoldierValidator.cs
@@ -0,0 +1,15 @@
+using BG.App.DTOs;
+using BG.Domain.Enums;
+using FluentValidation;
+
+namespace BG.App.Validators.Soldiers;
+
+public class CreateSoldierValidator : AbstractValidator<CreateSoldierRequest>
+{
+    public CreateSoldierValidator()
+    {
+        RuleFor(s => s.FirstName).NotEmpty().MaximumLength(50);
+        RuleFor(s => s.LastName).NotEmpty().MaximumLength(50);
+        RuleFor(s => s.Rank).IsEnumName(typeof(SoldierRank));
+    }
+}
---
+++ BG.App/Validators/Soldiers/UpdateSoldierValidator.cs
@@ -0,0 +1,16 @@
+using BG.App.DTOs;
+using BG.Domain.Enums;
+using FluentValidation;
+
+namespace BG.App.Validators.Soldiers;
+
+public class UpdateSoldierValidator : AbstractValidator<UpdateSoldierRequest>
+{
+    public UpdateSoldierValidator()
+    {
+        RuleFor(s => s.Id).NotEmpty();
+        RuleFor(s => s.FirstName).NotEmpty().MaximumLength(50);
+        RuleFor(s => s.LastName).NotEmpty().MaximumLength(50);
+        RuleFor(s => s.Rank).IsEnumName(typeof(SoldierRank));
+    }
+}
