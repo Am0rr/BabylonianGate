f77848478290358f6ac6228bd70e59891a908e3e
Am0rr <itsaizel468@gmail.com>
2026-01-29 16:08:28 +0200

    test(domain): implement unit tests for Soldier entity

---
+++ BG.Tests/Domain/Entities/SoldierTests.cs
@@ -0,0 +1,67 @@
+using BG.Domain.Entities;
+using BG.Domain.Enums;
+using FluentAssertions;
+
+namespace BG.Tests.Domain.Entities;
+
+public class SoldierTests
+{
+    [Fact]
+    public void Create_Should_ReturnSoldier_When_DataIsValid()
+    {
+        var firstName = "Benjamin";
+        var lastName = "Poindexter";
+        var rank = SoldierRank.Captain;
+
+        var soldier = Soldier.Create(firstName, lastName, rank);
+
+        soldier.FirstName.Should().Be("Benjamin");
+        soldier.LastName.Should().Be("Poindexter");
+        soldier.Rank.Should().Be(SoldierRank.Captain);
+    }
+
+    [Theory]
+    [InlineData("", "Poindexter")]
+    [InlineData("Benjamin", "")]
+    public void Create_Should_Throw_When_DataIsInvalid(string? firstName, string? lastName)
+    {
+        Action act = () => Soldier.Create(firstName!, lastName!, SoldierRank.Captain);
+
+        act.Should().Throw<ArgumentException>();
+    }
+
+    [Fact]
+    public void UpdateName_Should_UpdateFields_When_DataIsValid()
+    {
+        var soldier = Soldier.Create("Steve", "Rogers", SoldierRank.Private);
+
+        soldier.UpdateName("Captain", "America");
+
+        soldier.FirstName.Should().Be("Captain");
+        soldier.LastName.Should().Be("America");
+    }
+
+    [Theory]
+    [InlineData("", "Rogers")]
+    [InlineData("Steve", "")]
+    [InlineData(null, "Rogers")]
+    [InlineData("Steve", null)]
+    public void UpdateName_Should_Throw_When_DataIsInvalid(string? firstName, string? lastName)
+    {
+        var soldier = Soldier.Create("Steve", "Rogers", SoldierRank.Private);
+
+        Action act = () => soldier.UpdateName(firstName!, lastName!);
+
+        act.Should().Throw<ArgumentException>().WithMessage("Fist/Last Name cannot be empty.");
+    }
+
+    [Fact]
+    public void UpdateRank_Should_ChangeRank()
+    {
+        var soldier = Soldier.Create("Bucky", "Barnes", SoldierRank.Sergeant);
+
+        soldier.UpdateRank(SoldierRank.Captain);
+
+        soldier.Rank.Should().Be(SoldierRank.Captain);
+    }
+}
